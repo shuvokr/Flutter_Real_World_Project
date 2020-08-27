ea2d638bc259c496c97e6c57c1f08b766dd1f3e1
Shuvo <shuvo@Shuvos-MacBook-Air.local>
2020-08-28 03:14:48 +0600

    Initial commit : Flutter Real World Project.

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FlutterRealWorldApp());
+}
+
+class FlutterRealWorldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
