979deac48bd56925707c06fc691b3851aaf51bc2
Shuvo <shuvo@Shuvos-MacBook-Air.local>
2020-08-28 03:32:45 +0600

    Added a state full widget Added navigation bar Added progress indicator to center

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,25 @@ void main() {
   runApp(new FlutterRealWorldApp());
 }
 
-class FlutterRealWorldApp extends StatelessWidget {
+class FlutterRealWorldApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RealWorldState();
+  }
+}
+
+class RealWorldState extends State<FlutterRealWorldApp> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter Real World App"),
+        ),
+        body: new Center(
+          child: new CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
