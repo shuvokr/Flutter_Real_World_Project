1352c3f001b9dba71270a984f3dc1bc8e5e554f4
Shuvo <shuvo@Shuvos-MacBook-Air.local>
2020-08-29 20:37:02 +0600

    Added new detail view for each cell.

---
+++ lib/views/cell_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Detail Page"),
+      ),
+      body: new Center(
+        child: new Text("Hello Navigation"),
+      ),
+    );
+  }
+}
